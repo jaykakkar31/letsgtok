1765e07cab8a1a2177d20a1d5a34683684f7598e
jaykakkar31 <jaykakkar31@gmail.com>
2021-07-23 20:39:00 +0530

    completed

--- src/App.js
+++ src/App.js
@@ -5,8 +5,36 @@ import Header from "./components/header/header";
 import Body from "./components/Body/body";
 import Footer from "./components/footer/footer";
 import ContactUs from "./components/contactUs/contactUs";
-
+import { useEffect } from "react";
+import firebase from "firebase";
 function App() {
+	// const firebase = require("firebase");
+
+	firebase.initializeApp({
+		apiKey:
+			"BC1ma4RXohrNMNopPUpVVVoSnxCYT2nidA-YzSvDT8EjO8cA1kXfOz5bqNvLB7xu9IJ6v3x-nbxOFu1IHbfQ3mA",
+		authDomain: 
+        "letsgtok-1ce1a.firebaseapp.com",
+		projectId:
+         "letsgtok-1ce1a",
+	});
+	var firestore = firebase.firestore();
+	const handleClick = (user) => {
+        console.log("SAVE");
+		firestore
+			.collection("samples")
+			.add({
+				email: user.email,
+				name: user.name,
+				message: user.message,
+			})
+			.then((docRef) => {
+				console.log(docRef);
+			})
+			.catch((error) => {
+				console.error("Error adding document: ", error);
+			});
+	};
 	return (
 		<div>
 			<Router>
@@ -16,9 +44,9 @@ function App() {
 						<Body />
 						<Footer />
 					</Route>
-                    <Route exact path="/contactus">
-                        <ContactUs/>
-                    </Route>
+					<Route exact path="/contactus">
+						<ContactUs handleClick={handleClick} />
+					</Route>
 				</Switch>
 			</Router>
 		</div>
--- src/components/contactUs/contactUs.js
+++ src/components/contactUs/contactUs.js
@@ -1,33 +1,82 @@
-import './contactUs.css'
-import {Form,Button} from "react-bootstrap"
-const contactus=()=>{
-    return (
-			<div className="divForm">
-				<Form>
-					<Form.Group className="mb-3" controlId="formBasicEmail">
-						<Form.Label>Email address</Form.Label>
-						<Form.Control type="email" placeholder="Enter email" />
-						<Form.Text className="text-muted">
-							We'll never share your email with anyone else.
-						</Form.Text>
-					</Form.Group>
-					<Form.Group className="mb-3" controlId="formBasicEmail">
-						<Form.Label>Name</Form.Label>
-						<Form.Control type="email" placeholder="Enter email" />
-					</Form.Group>
-					<Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
-						<Form.Label>Message</Form.Label>
-						<Form.Control as="textarea" rows={3} />
-					</Form.Group>
-					<Form.Group className="mb-3" controlId="formBasicCheckbox">
-						<Form.Check type="checkbox" label="Check me out" />
-					</Form.Group>
-					<Button variant="primary" type="submit">
-						Submit
-					</Button>
-				</Form>
-			</div>
-		);
-}
+import "./contactUs.css";
+import { Form, Button } from "react-bootstrap";
+import { useState } from "react";
+const Contactus = ({ handleClick }) => {
+	const [user, setUser] = useState({
+		email: "",
+		name: "",
+		message: "",
+	});
+	const handleChange = (e) => {
+		console.log(e.target.name);
+		const { name, value } = e.target;
+		setUser((prevVal) => {
+			return { ...prevVal, [name]: value };
+		});
+	};
 
-export default contactus
+	return (
+		<div className="divForm">
+			<Form>
+				<Form.Group className="mb-3" controlId="formBasicEmail">
+					<Form.Label>Email address</Form.Label>
+					<Form.Control
+						onChange={handleChange}
+						type="email"
+						name="email"
+						value={user.email}
+						placeholder="Enter email"
+					/>
+					<Form.Text className="text-muted">
+						We'll never share your email with anyone else.
+					</Form.Text>
+				</Form.Group>
+				<Form.Group className="mb-3" controlId="formBasicEmail">
+					<Form.Label>Name</Form.Label>
+					<Form.Control
+						onChange={handleChange}
+						type="text"
+						name="name"
+						placeholder="Enter name"
+						value={user.name}
+					/>
+				</Form.Group>
+				<Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
+					<Form.Label>Message</Form.Label>
+					<Form.Control
+						onChange={handleChange}
+						name="message"
+						as="textarea"
+						rows={3}
+						value={user.message}
+					/>
+				</Form.Group>
+				
+				<Button
+					onClick={(event) => {
+						event.preventDefault();
+						if (user.name !== null && user.email !== null && user.message) {
+							handleClick(user);
+
+							setUser({
+								email: "",
+								name: "",
+								message: "",
+							});
+
+						}
+                        else{
+                            alert("Fields cannot be empty")
+                        }
+					}}
+					variant="primary"
+					type="submit"
+				>
+					Submit
+				</Button>
+			</Form>
+		</div>
+	);
+};
+
+export default Contactus;
